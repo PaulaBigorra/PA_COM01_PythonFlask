7bcadf16213224c906c1b87d901b0717dd8d8d54
Paula Bigorra <paula.an.bigo@gmail.com>
2025-06-08 15:58:52 -0300

    Create run.py

---
+++ Flask_project/run.py
@@ -0,0 +1,9 @@
+#Importa del modulo app la funcion create_app
+from app import create_app
+
+app = create_app() #Guarda la instancia como variable
+
+#Verifica si se jecuta directamente
+if __name__ == '__main__': 
+    app.run(debug=True)
+    #Inicia el servidor permitiendo acceder en el navegador
