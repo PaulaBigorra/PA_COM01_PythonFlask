3170fc0b5cb6689ddc3b57be574d0149e2fe8eaf
Paula Bigorra <paula.an.bigo@gmail.com>
2025-06-08 15:34:03 -0300

    Create config.py

---
+++ Flask_project/App/config.py
@@ -0,0 +1,4 @@
+#Se crea una clase para encapsular los ajustes globales 
+class Config:
+    SECRET_KEY = 'clave_secreta' #Asegura autenticaciones y sesiones
+    DEBUG = True #Muestra un mensaje de error detallado
